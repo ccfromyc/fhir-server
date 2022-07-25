b490a95db9d95df0e6e30829d3725b66bf73ad1d
Mecily <Mecily@gmail.com>
2022-07-25 23:10:33 +0800

    Second Add

--- src/Microsoft.Health.Fhir.Core.UnitTests/Features/Operations/Import/ImportOrchestratorJobTests.cs
+++ src/Microsoft.Health.Fhir.Core.UnitTests/Features/Operations/Import/ImportOrchestratorJobTests.cs
@@ -32,6 +32,7 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Operations.Import
         public async Task GivenAnOrchestratorJob_WhenProcessingInputFilesMoreThanConcurrentCount_ThenJobShouldBeCompleted()
         {
             await VerifyCommonOrchestratorJobAsync(100, 1);
+            await VerifyCommonOrchestratorJobAsync(100, 2);
             await VerifyCommonOrchestratorJobAsync(105, 6);
         }
 
